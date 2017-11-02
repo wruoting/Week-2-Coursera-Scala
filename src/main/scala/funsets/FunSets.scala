4225de71ffb282dc7986ee2bb2452af34abf444b
ruotingw <wruoting@gmail.com>
2017-11-02 01:48:26 -0400

    Final

--- src/main/scala/funsets/FunSets.scala
+++ src/main/scala/funsets/FunSets.scala
@@ -66,24 +66,30 @@ object FunSets {
    * Returns whether all bounded integers within `s` satisfy `p`.
    */
     def forall(s: Set, p: Int => Boolean): Boolean = {
-    def iter(a: Int): Boolean = {
-      if (contains(s,a)) true
-      else if (???) ???
-      else iter(???)
-    }
-    iter(bound)
+      def iter(a: Int): Boolean = {
+        if (a > bound) true
+        else if (contains(diff(s,p),a)) false
+        else iter(bound+1)
+      }
+      iter(-bound)
   }
   
   /**
    * Returns whether there exists a bounded integer within `s`
    * that satisfies `p`.
    */
-    def exists(s: Set, p: Int => Boolean): Boolean = ???
-  
+  //not all elements in s satisfies !p, which means at least one element in s satisfies p
+  //this doesn't care about a bound, it simply is a proof?
+    def exists(s: Set, p: Int => Boolean): Boolean = {!forall(s,x => !p(x))}
+
   /**
    * Returns a set transformed by applying `f` to each element of `s`.
    */
-    def map(s: Set, f: Int => Int): Set = ???
+  //let's say set2 = map(set1,f)
+  //the only way set2(x) is true is if there exists a y in set1 such that f(y) == x
+    def map(s: Set, f: Int => Int): Set = {
+      (a:Int) => exists(s,y => y == f(a))
+    }
   
   /**
    * Displays the contents of a set
